0418aacdc390b1b1c03afef3bcf143c3f064086e
Karan Srivastava <karan.srivastava@mongodb.com>
2021-03-26 13:21:52 +0530

    Initial check-in of module BooksApi

---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+#region snippet_NewtonsoftJsonImport
+using Newtonsoft.Json;
+#endregion
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        #region snippet_BookNameProperty
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+        #endregion
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+
+        [BsonExtraElements]
+        public IDictionary<string, object> Metadata { get; set; }
+    }
+}
